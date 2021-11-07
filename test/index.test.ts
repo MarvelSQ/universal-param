89fc9ab822759d949933672d789278822b374c29
MarvelSQ <sun1993hlj@hotmail.com>
2021-11-07 14:15:30 +0800

    feat(config): support create from config

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,32 @@
-import add from '../src';
+import dateFns from 'date-fns';
+import createParams from '../src';
 
-test('add number', () => {
-  expect(add(1, 2)).toBe(3);
+const DateConfig = {
+  parse(str: string) {
+    const date = new Date(str);
+    if (Number.isNaN(date.getTime())) {
+      return null;
+    }
+    return date;
+  },
+  format(date: Date) {
+    return dateFns.format(date, 'YYYY-MM-DD');
+  },
+};
+
+test('create params from config', () => {
+  const paramsConfig = {
+    dateType: ['DAILY', 'WEEKLY', 'MONTHLY'],
+    begin: DateConfig,
+    end: new Date(),
+    hasName: true,
+    word: undefined as unknown as string,
+  };
+  const globalParams = createParams(paramsConfig);
+
+  expect(globalParams.config.dateType).toEqual({
+    options: ['DAILY', 'WEEKLY', 'MONTHLY'],
+    default: 'DAILY',
+  });
+  expect(globalParams.config.end.default).toEqual(paramsConfig.end);
 });
